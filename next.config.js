cdbe47a023920bcfb76ed5bdc1acd106490b5100
Chris Buonocore <chrisdistrict@gmail.com>
2023-12-02 14:03:32 -0500

    update dependencies

--- next.config.js
+++ next.config.js
@@ -4,6 +4,9 @@ const nextConfig = {
     images: {
         unoptimized: true,
     },
+    experimental: {
+        serverComponentsExternalPackages: ['@trinsic/trinsic'],
+    },
     eslint: {
         // ignore eslint during build process
         ignoreDuringBuilds: true,
